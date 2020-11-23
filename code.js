2784d576fb23ea2a6402d4098f30c356ac3fdd51
buetowcj <cbuetow@my.wctc.edu>
2020-11-22 23:22:36 -0600

    Initial commit

---
+++ code.js
@@ -0,0 +1,20 @@
+$(document).ready(
+    function () {
+        $("form").submit(placeOrder);
+
+        function placeOrder(event)
+        {
+            event.preventDefault();
+            if (x)
+            {
+
+            }
+            else if (x)
+            {
+
+            }
+
+        }
+
+    }
+);
