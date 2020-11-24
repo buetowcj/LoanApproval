a3399914359aab71a21702572c9374c14d6ee081
buetowcj <cbuetow@my.wctc.edu>
2020-11-24 14:56:23 -0600

    Final commit

--- code.js
+++ code.js
@@ -5,13 +5,26 @@ $(document).ready(
         function placeOrder(event)
         {
             event.preventDefault();
-            if (x)
-            {
 
+            var salary = parseFloat($("#salary").val());
+            var creditScore = parseFloat($("#creditScore").val());
+            var monthsAtJob = parseFloat($("#monthsAtJob").val());
+
+            if (salary >= 40000 && creditScore >= 600)
+            {
+                $("#output").text("Loan Approved!")
             }
-            else if (x)
+            else if (salary < 40000 && monthsAtJob >= 12)
             {
-
+                $("#output").text("Loan Approved!")
+            }
+            else if (creditScore >= 600 && monthsAtJob >= 12)
+            {
+                $("#output").text("Loan Approved!")
+            }
+            else
+            {
+                $("#output").text("Loan Denied :(")
             }
 
         }
